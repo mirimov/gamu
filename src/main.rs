a24cdb70aa0cdfa805a90240357bcaafb34adc3e
Miri <miri.mov.media@gmail.com>
2026-03-22 17:08:37 -0400

    now with buster logic and shots flying

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
-// If the target architecture is 16-bit (like the Game Boy)
-#[cfg(target_pointer_width = "16")]
-pub type Int = u8;
-
-// If the target architecture is 64-bit (like your Linux machine)
-#[cfg(target_pointer_width = "64")]
-pub type Int = i32;
-
 fn main() {
+  let _s = gamu::logic::start();
   println!("Hello, world!");
 }
